04fdcc4d5f3d37b8a3441ed0d804361fc9679715
leohjx <hojin421@gmail.com>
2021-10-26 16:08:44 +1100

    add tcp 2way with modem trace example

---
+++ samples/nrf9160/lp_2way_udp_mdm_trace/src/main.c
@@ -0,0 +1,359 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <modem/lte_lc.h>
+#include <nrf_socket.h>
+#include <net/socket.h>
+
+#define UDP_IP_HEADER_SIZE 28
+
+static int client_fd;
+static int client_fd_RAI;
+static struct sockaddr_storage host_addr;
+static struct k_work_delayable server_transmission_work;
+
+K_SEM_DEFINE(lte_connected, 0, 1);
+
+static int server_connect(void);
+
+void socket_poll_test(int infd)
+{
+    struct pollfd fd;
+    int ret;
+    int cnt = 0;
+
+	fd.fd = infd;
+	fd.events = POLLIN;
+    while(cnt++ <4){
+        ret = poll(&fd, 1, 10);
+        if (ret < 0) {
+            printk("poll() error: %d", -errno);
+            ret = -EIO;
+            //break;
+        } /* IO error */
+        if (ret == 0) {
+          // nothing for now. 
+        } /* timeout */
+        printk("%d  fd events 0x%08x\n", cnt,  fd.revents);
+    }
+}
+
+static void server_transmission_work_fn(struct k_work *work)
+{
+	int err;
+    static uint32_t cnt;
+
+	char buffer[CONFIG_UDP_DATA_UPLOAD_SIZE_BYTES] = {"#"};
+
+    memset(buffer, '#', CONFIG_UDP_DATA_UPLOAD_SIZE_BYTES);
+
+#if defined(CONFIG_RAI_ENABLE_AFTER_PACKET_SEND)	
+	err = lte_lc_rai_req(false);
+	if (err) {
+		printk("lte_lc_rai_req, error: %d\n", err);
+	}
+
+	//k_sleep(K_MSEC(100));
+
+	err = server_connect();
+	if (err) {
+		printk("Not able to connect to UDP server\n");
+		return;
+	}
+#endif	/* #if defined(CONFIG_RAI_ENABLE_AFTER_PACKET_SEND)  */
+#if defined(CONFIG_RECEIVE_UDP_PACKETS)
+    cnt=0;
+    sprintf(buffer, "pkg:%d ", cnt);  /* fixed packets for server to check and reply.  */
+#else
+    sprintf(buffer, "pkg:%d ", cnt++);
+#endif
+
+	printk("Transmitting UDP/IP payload of %d bytes to the ",
+	       CONFIG_UDP_DATA_UPLOAD_SIZE_BYTES + UDP_IP_HEADER_SIZE);
+	printk("IP address %s, port number %d\n",
+	       CONFIG_UDP_SERVER_ADDRESS_STATIC,
+	       CONFIG_UDP_SERVER_PORT);
+
+	err = send(client_fd, buffer, sizeof(buffer), 0);
+	if (err < 0) {
+		printk("Failed to transmit UDP packet, %d\n", errno);
+		return;
+	}
+#if defined(CONFIG_RECEIVE_UDP_PACKETS)
+
+    memset(buffer, 0, sizeof(buffer));
+
+	err = recv(client_fd, buffer, CONFIG_UDP_DATA_UPLOAD_SIZE_BYTES, 0);
+	if (err < 0) {
+		printk("Failed to receive UDP packet, %d\n", errno);
+        goto workerror;
+		/* return; */ /* do not return */
+	}  
+    printk("recv: %s\n", buffer);
+    socket_poll_test(client_fd);
+    close(client_fd);
+    socket_poll_test(client_fd);
+
+	k_sleep(K_MSEC(600));
+    
+workerror:
+#endif
+#if defined(CONFIG_RAI_ENABLE_AFTER_PACKET_SEND)
+	/** Release Assistance Indication  */
+	err = lte_lc_rai_req(true);
+	if (err) {
+		printk("lte_lc_rai_req, error: %d\n", err);
+	}
+	
+	err = send(client_fd_RAI, buffer, 2, 0);
+	if (err < 0) {
+		printk("Failed to transmit UDP packet, %d\n", errno);
+		return;
+	}
+    k_sleep(K_MSEC(20));
+    close(client_fd_RAI);
+#endif	
+
+	k_work_schedule(&server_transmission_work,
+			K_SECONDS(CONFIG_UDP_DATA_UPLOAD_FREQUENCY_SECONDS));
+}
+
+static void work_init(void)
+{
+	k_work_init_delayable(&server_transmission_work,
+			      server_transmission_work_fn);
+}
+
+#if defined(CONFIG_NRF_MODEM_LIB)
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+	switch (evt->type) {
+	case LTE_LC_EVT_NW_REG_STATUS:
+		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+		     (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+			break;
+		}
+
+		printk("Network registration status: %s\n",
+			evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+			"Connected - home network" : "Connected - roaming\n");
+		k_sem_give(&lte_connected);
+		break;
+	case LTE_LC_EVT_PSM_UPDATE:
+		printk("PSM parameter update: TAU: %d, Active time: %d\n",
+			evt->psm_cfg.tau, evt->psm_cfg.active_time);
+		break;
+	case LTE_LC_EVT_EDRX_UPDATE: {
+		char log_buf[60];
+		ssize_t len;
+
+		len = snprintf(log_buf, sizeof(log_buf),
+			       "eDRX parameter update: eDRX: %f, PTW: %f\n",
+			       evt->edrx_cfg.edrx, evt->edrx_cfg.ptw);
+		if (len > 0) {
+			printk("%s\n", log_buf);
+		}
+		break;
+	}
+	case LTE_LC_EVT_RRC_UPDATE:
+		printk("RRC mode: %s\n",
+			evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ?
+			"Connected" : "Idle\n");
+		break;
+	case LTE_LC_EVT_CELL_UPDATE:
+		printk("LTE cell changed: Cell ID: %d, Tracking area: %d\n",
+		       evt->cell.id, evt->cell.tac);
+		break;
+	default:
+		break;
+	}
+}
+
+static int configure_low_power(void)
+{
+	int err;
+
+#if defined(CONFIG_UDP_PSM_ENABLE)
+	/** Power Saving Mode */
+	err = lte_lc_psm_req(true);
+	if (err) {
+		printk("lte_lc_psm_req, error: %d\n", err);
+	}
+#else
+	err = lte_lc_psm_req(false);
+	if (err) {
+		printk("lte_lc_psm_req, error: %d\n", err);
+	}
+#endif
+
+#if defined(CONFIG_UDP_EDRX_ENABLE)
+	/** enhanced Discontinuous Reception */
+	err = lte_lc_edrx_req(true);
+	if (err) {
+		printk("lte_lc_edrx_req, error: %d\n", err);
+	}
+#else
+	err = lte_lc_edrx_req(false);
+	if (err) {
+		printk("lte_lc_edrx_req, error: %d\n", err);
+	}
+#endif
+
+#if defined(CONFIG_UDP_RAI_ENABLE)
+	/** Release Assistance Indication  */
+	err = lte_lc_rai_req(true);
+	if (err) {
+		printk("lte_lc_rai_req, error: %d\n", err);
+	}
+#endif
+
+	return err;
+}
+
+static void modem_init(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already configured and LTE connected. */
+	} else {
+		err = lte_lc_init();
+		if (err) {
+			printk("Modem initialization failed, error: %d\n", err);
+			return;
+		}
+	}
+}
+
+static void modem_connect(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already configured and LTE connected. */
+	} else {
+		err = lte_lc_connect_async(lte_handler);
+		if (err) {
+			printk("Connecting to LTE network failed, error: %d\n",
+			       err);
+			return;
+		}
+	}
+}
+#endif
+
+static void server_disconnect(void)
+{
+	(void)close(client_fd);
+    (void)close(client_fd_RAI);
+}
+
+static int server_init(void)
+{
+	struct sockaddr_in *server4 = ((struct sockaddr_in *)&host_addr);
+
+	server4->sin_family = AF_INET;
+	server4->sin_port = htons(CONFIG_UDP_SERVER_PORT);
+
+	inet_pton(AF_INET, CONFIG_UDP_SERVER_ADDRESS_STATIC,
+		  &server4->sin_addr);
+
+	return 0;
+}
+
+static int server_connect(void)
+{
+	int err;
+
+#if defined(CONFIG_TEST_USE_TCP)
+	client_fd = socket(AF_INET, SOCK_STREAM /* SOCK_DGRAM */, IPPROTO_TCP /* IPPROTO_UDP*/);
+    client_fd_RAI = socket(AF_INET,  SOCK_DGRAM , IPPROTO_UDP);;
+#else
+	client_fd = socket(AF_INET,  SOCK_DGRAM , IPPROTO_UDP);
+#endif
+	if (client_fd < 0) {
+		printk("Failed to create UDP socket: %d\n", errno);
+		err = -errno;
+		goto error;
+	}
+	if (client_fd_RAI < 0) {
+		printk("Failed to create UDP socket: %d\n", errno);
+		err = -errno;
+		goto error;
+	}
+#if defined(CONFIG_UDP_RECV_TIMEOUT_SECONDS)
+	struct timeval timeout = {
+		.tv_sec = CONFIG_UDP_RECV_TIMEOUT_SECONDS,
+		.tv_usec = 0,
+	};
+
+	err = setsockopt(client_fd,
+			 NRF_SOL_SOCKET,
+			 NRF_SO_RCVTIMEO,
+			 &timeout,
+			 sizeof(timeout));
+	if (err) {
+		printk("Failed to setup socket timeout, errno %d\n", errno);
+		/* do not return */ /* return -1; */
+	}
+#endif
+	err = connect(client_fd, (struct sockaddr *)&host_addr,
+		      sizeof(struct sockaddr_in));
+	if (err < 0) {
+		printk("Connect failed : %d\n", errno);
+		goto error;
+	}
+	err = connect(client_fd_RAI, (struct sockaddr *)&host_addr,
+		      sizeof(struct sockaddr_in));
+	if (err < 0) {
+		printk("Connect failed : %d\n", errno);
+		goto error;
+	}
+	return 0;
+
+error:
+	server_disconnect();
+
+	return err;
+}
+
+void main(void)
+{
+	int err;
+
+	printk("UDP sample has started\n");
+
+	work_init();
+
+#if defined(CONFIG_NRF_MODEM_LIB)
+
+	/* Initialize the modem before calling configure_low_power(). This is
+	 * because the enabling of RAI is dependent on the
+	 * configured network mode which is set during modem initialization.
+	 */
+	modem_init();
+
+	err = configure_low_power();
+	if (err) {
+		printk("Unable to set low power configuration, error: %d\n",
+		       err);
+	}
+
+	modem_connect();
+
+	k_sem_take(&lte_connected, K_FOREVER);
+#endif
+
+	err = server_init();
+	if (err) {
+		printk("Not able to initialize UDP server connection\n");
+		return;
+	}
+
+	k_work_schedule(&server_transmission_work, K_NO_WAIT);
+}
