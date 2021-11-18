69b0ff8af98eea562ec1d3c099224ea0eb5f29fb
LeoHJX <hojin421@gmail.com>
2021-11-18 22:09:29 +1100

    add modem onoff test add large data upload + ack with RAI test case

--- samples/nrf9160/lp_udp_fun_1_4/batchBuild.sh
+++ samples/nrf9160/lp_udp_fun_1_4/batchBuild.sh
@@ -31,4 +31,3 @@ mkdir ./output_hexs/m1_b3_8_28_dbg_trace
 west build -b nrf9160dk_nrf9160_ns -p -- -DOVERLAY_CONFIG=m1_b3_8_28_dbg_trace.conf
 mv build/zephyr/merged.hex ./output_hexs/m1_b3_8_28_dbg_trace
 cp ./flash_nrf9160_merged.hex.bat ./output_hexs/m1_b3_8_28_dbg_trace
-
