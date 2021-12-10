7d991cc7b7b85519bbc816f82a9090c739da1244
LeoHJX <hojin421@gmail.com>
2021-12-10 12:43:03 +1100

    create first nrfx base project, switch off modem.

---
+++ samples/nrf9160/nrfx/modem_off_only/README.rst
@@ -0,0 +1,11 @@
+.. modem off:
+
+nRF9160: modem off example
+############
+
+.. contents::
+   :local:
+   :depth: 2
+
+Demo to switch off modem for low power, as all nrfx examples won't require modem, so to turn off first. 
+
---
+++ samples/nrf9160/nrfx/modem_off_only/src/main.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <modem/lte_lc.h>
+
+#if 0
+static void modem_init(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already configured and LTE connected. */
+	} else {
+		err = lte_lc_init();
+		if (err) {
+			printk("Modem initialization failed, error: %d\n", err);
+			return;
+		}
+	}
+}
+#endif
+void main(void)
+{
+	int err;
+#if 0
+	modem_init();
+#endif
+	lte_lc_func_mode_set(LTE_LC_FUNC_MODE_NORMAL);
+	printk("Just switch Modem Off\n"); /* should output to RTT*/
+	err = lte_lc_func_mode_set(LTE_LC_FUNC_MODE_OFFLINE);
+	if(err < 0 ){
+		printk("lte_lc_func_mode_set error %d\n", err);
+	}
+#if 0
+	while(1)
+	{
+		k_cpu_idle();
+		k_sleep(K_MSEC(5000));
+	}
+#endif
+	return;
+}
