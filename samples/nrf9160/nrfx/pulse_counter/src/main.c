78fdc4b55a54202ef8d0d0e787c484f4bbab86c5
LeoHJX <hojin421@gmail.com>
2021-12-10 15:52:56 +1100

    add pulse counter

---
+++ samples/nrf9160/nrfx/pulse_counter/src/main.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <modem/lte_lc.h>
+
+#include <nrf9160.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <nrfx_timer.h>
+#include <nrfx_dppi.h>
+#include <nrfx_gpiote.h>
+#include <hal/nrf_gpiote.h>
+#include <hal/nrf_timer.h>
+
+#include <hal/nrf_gpio.h>
+
+const uint32_t counting_pin = 6;
+const uint32_t counter_read_pin = 7;
+
+static const nrfx_timer_t timer = NRFX_TIMER_INSTANCE(0);
+
+static void gpiote_event_handler(nrfx_gpiote_pin_t pin,
+				 nrf_gpiote_polarity_t action)
+{
+	if (pin == counter_read_pin) {
+		/* Read out timer CC register and clear it */
+		uint32_t timer_cc_val = nrf_timer_cc_get(timer.p_reg, 0);   /* change read to get */
+		printk("Raw counter val: %d\n", timer_cc_val);
+		/* nrfx_timer_clear(&timer);  */ /* call this one to clear the counter */
+	} else {
+		printk("Unknown pin. Check your button configuration\n");
+	}
+}
+
+static void timer_handler(nrf_timer_event_t event_type, void *context)
+{
+   /* do nothing */
+}
+
+static void timer_init(void)
+{
+	nrfx_timer_config_t timer_config = NRFX_TIMER_DEFAULT_CONFIG;
+	timer_config.bit_width = TIMER_BITMODE_BITMODE_32Bit;
+	timer_config.mode = NRF_TIMER_MODE_COUNTER;
+	//timer_config.frequency = NRF_TIMER_FREQ_16MHz; // don't care in counter mode. 
+	timer_config.mode = NRF_TIMER_MODE_COUNTER;
+
+	uint32_t err = nrfx_timer_init(&timer, &timer_config, timer_handler);
+	if (err != NRFX_SUCCESS) {
+		printk("Err :%x\n", err);
+	}
+	nrfx_timer_enable(&timer);
+}
+
+static void dppi_init(void)
+{
+	uint8_t dppi_ch_1, dppi_ch_2;
+	uint32_t err = nrfx_dppi_channel_alloc(&dppi_ch_1);
+	if (err != NRFX_SUCCESS) {
+		printk("Err %d\n", err);
+		return;
+	}
+
+	err = nrfx_dppi_channel_alloc(&dppi_ch_2);
+	if (err != NRFX_SUCCESS) {
+		printk("Err %d\n", err);
+		return;
+	}
+
+	err = nrfx_dppi_channel_enable(dppi_ch_1);
+	if (err != NRFX_SUCCESS) {
+		printk("Err %d\n", err);
+		return;
+	}
+
+	err = nrfx_dppi_channel_enable(dppi_ch_2);
+	if (err != NRFX_SUCCESS) {
+		printk("Err %d\n", err);
+		return;
+	}
+
+	/* Tie it all together */
+	nrf_gpiote_publish_set(NRF_GPIOTE1, NRF_GPIOTE_EVENT_IN_0, dppi_ch_1); /* new name change */
+	nrf_gpiote_publish_set(NRF_GPIOTE1, NRF_GPIOTE_EVENT_IN_1,  dppi_ch_2);
+
+	nrf_timer_subscribe_set(timer.p_reg, NRF_TIMER_TASK_COUNT, dppi_ch_1);
+	nrf_timer_subscribe_set(timer.p_reg, NRF_TIMER_TASK_CAPTURE0, dppi_ch_2);
+}
+
+static void gpiote_init(void)
+{
+	nrfx_gpiote_in_config_t falling_edge = { 0 };
+	nrfx_gpiote_in_config_t rising_edge = { 0 };
+
+	falling_edge.hi_accuracy = 1;
+	falling_edge.pull = NRF_GPIO_PIN_PULLUP;
+	falling_edge.sense = NRF_GPIOTE_POLARITY_LOTOHI;
+
+	rising_edge.hi_accuracy = 1;
+	rising_edge.pull = NRF_GPIO_PIN_PULLUP;
+	rising_edge.sense = NRF_GPIOTE_POLARITY_HITOLO;
+
+	uint32_t err = nrfx_gpiote_init(0);  /* pass interupt prioritity */
+	if (err != NRFX_SUCCESS) {
+		printk("gpiote1 err: %x", err);
+		return;
+	}
+
+	err = nrfx_gpiote_in_init(counting_pin, &rising_edge, NULL);
+	if (err != NRFX_SUCCESS) {
+		printk("gpiote1 err: %x", err);
+		return;
+	}
+
+	err = nrfx_gpiote_in_init(counter_read_pin, &falling_edge,
+				  gpiote_event_handler);
+	if (err != NRFX_SUCCESS) {
+		printk("gpiote2 err: %x", err);
+		return;
+	}
+
+	nrfx_gpiote_in_event_enable(counter_read_pin, 1);
+	nrfx_gpiote_in_event_enable(counting_pin, 1);
+}
+
+static void manual_isr_setup()
+{
+	IRQ_DIRECT_CONNECT(GPIOTE1_IRQn, 0, nrfx_gpiote_irq_handler, 0);
+	irq_enable(GPIOTE1_IRQn);
+}
+
+void main(void)
+{
+	int err;
+#if 0
+	modem_init();
+#endif
+	lte_lc_func_mode_set(LTE_LC_FUNC_MODE_NORMAL);
+	printk("Just switch Modem Off\n"); /* should output to RTT*/
+	err = lte_lc_func_mode_set(LTE_LC_FUNC_MODE_OFFLINE);
+	if(err < 0 ){
+		printk("lte_lc_func_mode_set error %d\n", err);
+	}
+
+	printk("This sample requires DPPIC to be set as nonsecure\n");
+
+	printk("This sample takes in a signal,"
+	       "and uses two GPIOTE IN channels to time it via TIMER0 and DPPIC\n");
+	timer_init();
+	dppi_init();
+	gpiote_init();
+	manual_isr_setup();
+}
